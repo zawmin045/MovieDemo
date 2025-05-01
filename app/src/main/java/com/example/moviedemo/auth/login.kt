5a73accee1666fae18251dc63c2f9ccc7cbd4f9e
zaw min oo <zawyuk020@gmail.com>
2025-05-01 18:37:12 +0700

    login-screen zaw

---
+++ app/src/main/java/com/example/moviedemo/auth/login.kt
@@ -0,0 +1,16 @@
+package com.example.moviedemo.auth
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.example.moviedemo.R
+
+class login : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+
+    }
+}
